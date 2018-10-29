5130ffbfe69174d17cc4bb5bc921bc8ef86eaebf
Yohan Graterol <yohangraterol92@gmail.com>
2018-10-29 00:20:25 -0500

    Fix genesis block commit

--- core/genesis.go
+++ core/genesis.go
@@ -193,7 +193,7 @@ func SetupGenesisBlockWithOverride(db ethdb.Database, genesis *Genesis, constant
 	// Special case: don't change the existing config of a non-mainnet chain if no new
 	// config is supplied. These chains would get AllProtocolChanges (and a compat error)
 	// if we just continued here.
-	if genesis == nil && stored != params.MainnetGenesisHash {
+	if genesis == nil && stored != params.CallistoGenesisHash {
 		return storedcfg, stored, nil
 	}
 
@@ -215,8 +215,8 @@ func (g *Genesis) configOrDefault(ghash common.Hash) *params.ChainConfig {
 	switch {
 	case g != nil:
 		return g.Config
-	case ghash == params.MainnetGenesisHash:
-		return params.MainnetChainConfig
+	case ghash == params.CallistoGenesisHash:
+		return params.CallistoChainConfig
 	case ghash == params.TestnetGenesisHash:
 		return params.TestnetChainConfig
 	default:
