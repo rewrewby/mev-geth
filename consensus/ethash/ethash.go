5b174eb8c50f9df5f295e43c074b677998caa723
Admin <admin@k1pool.com>
2021-05-22 10:40:18 +0300

    consensus: don't use mmap if cache/dataset disk item is not allowed

--- consensus/ethash/ethash.go
+++ consensus/ethash/ethash.go
@@ -238,7 +238,7 @@ func (c *cache) generate(dir string, limit int, lock bool, test bool) {
 			size = 1024
 		}
 		// If we don't store anything on disk, generate and return.
-		if dir == "" {
+		if dir == "" || limit <= 0 {
 			c.cache = make([]uint32, size/4)
 			generateCache(c.cache, c.epoch, seed)
 			return
@@ -320,7 +320,7 @@ func (d *dataset) generate(dir string, limit int, lock bool, test bool) {
 			dsize = 32 * 1024
 		}
 		// If we don't store anything on disk, generate and return
-		if dir == "" {
+		if dir == "" || limit <= 0 {
 			cache := make([]uint32, csize/4)
 			generateCache(cache, d.epoch, seed)
 
